9830262599d44b3240eba13e7fe3c4e3999ae588
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-06-14 14:25:50 +0600

    working on cursor

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+// Create Mouse Cursor
+const mouseCursor = document.createElement('div');
+mouseCursor.id = "cursor";
+document.body.appendChild(mouseCursor);
+
+
+// Cursor Move With Mouse
+window.addEventListener('mousemove', function (e) {
+  mouseCursor.style.top = `${e.clientY}px`;
+  mouseCursor.style.left = `${e.clientX}px`;
+});
