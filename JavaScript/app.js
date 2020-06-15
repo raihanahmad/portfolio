e8fc8008bcccca6259669fdfc3cf5b8f15a2e0bd
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-06-15 08:50:45 +0600

    litte change in cursor and navbar

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,28 +1,26 @@
 // Target UI Element
-const showcaseTitle = document.querySelector('#name-title > div');
-const blackWhiteToggler = document.querySelector('#arrow-svg');
-const navLi = document.querySelectorAll('#navbar-links > ul > li');
-
+const showcaseTitle = document.querySelector("#name-title > div");
+const blackWhiteToggler = document.querySelector("#arrow-svg");
+const navLi = document.querySelectorAll("#navbar-links > ul > li");
 
 // Create Mouse Cursor
-const mouseCursor = document.createElement('div');
+const mouseCursor = document.createElement("div");
 mouseCursor.className = "cursor";
 document.body.appendChild(mouseCursor);
 
-
 // Cursor Move With Mouse
-window.addEventListener('mousemove', function (e) {
+window.addEventListener("mousemove", function (e) {
   mouseCursor.style.top = `${e.clientY}px`;
   mouseCursor.style.left = `${e.clientX}px`;
 });
 
 // Landing Page Title Hover and  Cursor Big
-showcaseTitle.addEventListener('mouseleave', function (e) {
-  mouseCursor.classList.remove('cursorGrowOnTitle');
+showcaseTitle.addEventListener("mouseleave", function (e) {
+  mouseCursor.classList.remove("cursorGrowOnTitle");
 });
 
-showcaseTitle.addEventListener('mouseover', function (e) {
-  mouseCursor.classList.add('cursorGrowOnTitle');
+showcaseTitle.addEventListener("mouseover", function (e) {
+  mouseCursor.classList.add("cursorGrowOnTitle");
 });
 
 // // Black And White Mode Controler
@@ -32,39 +30,30 @@ showcaseTitle.addEventListener('mouseover', function (e) {
 
 // Navbar Magnet Effect
 (function () {
-
   const navbarMagnetEffect = function (e) {
-    const navLink = this.querySelector('#navbar-links .nav-link');
+    const navLink = this.querySelector("#navbar-links .nav-link");
 
-    const {
-      offsetX: x,
-      offsetY: y
-    } = e, {
-      offsetWidth: width,
-      offsetHeight: height
-    } = this,
-
-    move = 25,
-      xMove = x / width * (move * 3) - move,
-      yMove = y / height * (move * 3) - move;
+    const { offsetX: x, offsetY: y } = e,
+      { offsetWidth: width, offsetHeight: height } = this,
+      move = 25,
+      xMove = (x / width) * (move * 3) - move,
+      yMove = (y / height) * (move * 3) - move;
 
     navLink.style.transform = `translate(${xMove}px, ${yMove}px)`;
-    mouseCursor.classList.add('navHoverCursor');
+    mouseCursor.classList.add("navHoverCursor");
 
+    if (e.type === "mouseleave") {
+      navLink.style.transform = "";
+      mouseCursor.classList.remove("navHoverCursor");
+    }
+  };
 
-    if (e.type === 'mouseleave') {
-      navLink.style.transform = '';
-      mouseCursor.classList.remove('navHoverCursor');
-
-    };
-  }
-
-  navLi.forEach(b => {
-    b.addEventListener('mousemove', navbarMagnetEffect);
+  navLi.forEach((b) => {
+    b.addEventListener("mousemove", navbarMagnetEffect);
     console.log(mouseCursor);
   });
-  navLi.forEach(b => {
-    b.addEventListener('mouseleave', navbarMagnetEffect);
+  navLi.forEach((b) => {
+    b.addEventListener("mouseleave", navbarMagnetEffect);
   });
 })();
 
