da2e4eb89937ba37b2f6d11880b925e6a59c5300
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-06-20 15:30:11 +0600

    smooth scrolling added

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -124,4 +124,24 @@ const distortEffect = function () {
 
 distortEffect();
 
-console.log(projectsSection);
+
+// For Smoot Scroolling
+const body = document.body,
+  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+  height = scrollWrap.getBoundingClientRect().height - 1,
+  speed = 0.1;
+
+var offset = 0;
+
+body.style.height = Math.floor(height) + "px";
+
+function smoothScroll() {
+  offset += (window.pageYOffset - offset) * speed;
+
+  var scroll = "translateY(-" + offset + "px) translateZ(0)";
+  scrollWrap.style.transform = scroll;
+
+  callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
